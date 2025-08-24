70509afe460ab3e3161a07384e4e732ac0524a68
n7n788 <yuki1113.2001@gmail.com>
2025-08-24 13:04:55 +0900

    fix: 同一ディレクトリにmain関数を複数配置できないのでサブディレクトリに移動

---
+++ 7-8/client/client.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+  "crypto/tls"
+	"crypto/x509"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+)
+
+func main() {
+	// 証明書を読み込む
+	cert, err := os.ReadFile("../ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(cert)
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	// クライアントを作成
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	// 通信を行う
+	resp, err := client.Get("https://localhost:18443")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
+}
